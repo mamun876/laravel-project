226496fe590e9d5e8e770c94925a0a16b040f281
Mamun876 <mamunrrashid876@gmail.com>
2024-01-26 10:11:52 +0600

    Your commit message here

--- resources/views/backend/categorycreate.blade.php
+++ resources/views/backend/categorycreate.blade.php
@@ -1,9 +1,12 @@
 @extends('backend.layouts.app')
 @section('title', 'Home Page')
 @section('content')
-    <form action="{{route('category.store')}}" method="post" enctype="multipart/form-data" style="width: 50%;">
-        @csrf
-        <input type="text" name="name" placeholder="name" class="form-control" value="{{old('name')}}">
-         <input type="submit" value="Submit" class="btn btn-success">
-    </form>
+<form action="{{ route('category.store') }}" method="post" enctype="multipart/form-data" style="width: 50%;">
+    @csrf
+    <input type="text" name="category_name" placeholder="Category Name" class="form-control" value="{{ old('category_name') }}">
+    <input type="text" name="category_code" placeholder="Category Code" class="form-control" value="{{ old('category_code') }}">
+    <input type="text" name="description" placeholder="Description" class="form-control" value="{{ old('description') }}">
+    <input type="text" name="created_by" placeholder="Created By" class="form-control" value="{{ old('created_by') }}">
+    <input type="submit" value="Submit" class="btn btn-success">
+</form>
 @endsection
